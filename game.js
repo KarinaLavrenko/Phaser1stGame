7e923630e3d6b430bd07c6a858585e4ec00912ec
KarinaLavrenko <158204760+KarinaLavrenko@users.noreply.github.com>
2024-02-07 22:17:16 +0200

    Додали платформи

--- game.js
+++ game.js
@@ -8,12 +8,20 @@ var config = {
         update: update
     }
 };
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
-    //Додали платформу
+    //Додали асети
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
@@ -26,8 +34,16 @@ function preload ()
 
 function create ()
 {
+    //Додали небо
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
+    //Додали платформи
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
