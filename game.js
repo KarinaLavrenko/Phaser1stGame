4c8f1e5b7b3d323c6a2d3173ecd1d2f8c8e70d2f
KarinaLavrenko <158204760+KarinaLavrenko@users.noreply.github.com>
2024-02-07 14:59:23 +0200

    Додали папку style.css в index.html

--- game.js
+++ game.js
@@ -2,14 +2,6 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    //Додали зелені платформи
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    },
     scene: {
         preload: preload,
         create: create,
@@ -19,30 +11,14 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {
-    //Завантаження ресурсів та спрайтів
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude',
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
+function preload ()
+{
 }
 
-function create() {
-    //Додали небо
-    this.add.image(400, 300, 'sky');
-    //Додали платформу
-    platforms = this.physics.add.staticGroup();
-
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+function create ()
+{
 }
 
-function update() {
+function update ()
+{
 }
